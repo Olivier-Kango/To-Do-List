937a6ac03035416a05b6e1451fb94ea6bbab1e7e
Olivier-Kango <olivierkango@gmail.com>
2022-10-06 14:37:20 -0700

    AddEventLister to AddBtn

--- src/index.js
+++ src/index.js
@@ -1,19 +1,35 @@
 /* eslint-disable no-unused-vars */
 import _, { add } from 'lodash';
 import './style.css';
-import addTask, { AddItem } from './addremove.js';
+import addTask, { AddItem, index } from './addremove.js';
 /* eslint-enable no-unused-vars */
 
-// let toDoList = [];
+let toDoList = [];
 
 // Add and Remove
 const AddBtn = document.querySelector('.add-items-btn');
 
 AddBtn.onclick = addTask;
 
+AddBtn.addEventListener('click', () => {
+  AddBtn.onclick = addTask;
+  const hash = {
+    description: AddItem.value,
+    completed: false,
+    index: index,
+  };
+  toDoList.push(hash);
+});
+
 AddItem.addEventListener('keypress', (enter) => {
   if (enter.key === 'Enter') {
     enter.preventDefault();
     AddBtn.onclick();
+    const hash = {
+      description: AddItem.value,
+      completed: false,
+      index: index,
+    };
+    toDoList.push(hash);
   }
 });
