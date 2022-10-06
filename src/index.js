eb0c79411edfa8149c61275e0190ce589a39c27e
Olivier-Kango <olivierkango@gmail.com>
2022-10-06 12:54:29 -0700

    import addTask and AddItem

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 /* eslint-disable no-unused-vars */
 import _, { add } from 'lodash';
 import './style.css';
-import addTask from './addremove.js';
+import addTask, { AddItem } from './addremove.js';
 /* eslint-enable no-unused-vars */
 
 // let toDoList = [];
 
 // Add and Remove
 const AddBtn = document.querySelector('.add-items-btn');
-const AddItem = document.getElementById('text-input');
 
 AddBtn.onclick = addTask;
 
