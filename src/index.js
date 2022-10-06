75bf1f933d3c42ba0ff50d14d08e7280c513d18f
Olivier-Kango <olivierkango@gmail.com>
2022-10-06 15:18:25 -0700

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -4,32 +4,15 @@ import './style.css';
 import addTask, { AddItem } from './addremove.js';
 /* eslint-enable no-unused-vars */
 
-let toDoList = [];
+// let toDoList = [];
 
 // Add and Remove
 const AddBtn = document.querySelector('.add-items-btn');
 
 AddBtn.onclick = addTask;
 
-AddBtn.addEventListener('click', () => {
-  AddBtn.onclick = addTask;
-  const hash = {
-    description: AddItem.value,
-    completed: false,
-    index: index,
-  };
-  toDoList.push(hash);
-});
-
 AddItem.addEventListener('keypress', (enter) => {
   if (enter.key === 'Enter') {
     enter.preventDefault();
-    AddBtn.onclick();
-    const hash = {
-      description: AddItem.value,
-      completed: false,
-      index: index,
-    };
-    toDoList.push(hash);
   }
 });
