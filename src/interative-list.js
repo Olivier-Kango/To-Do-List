55c8d571bc1061f60e0bda3a5334fa5057030197
Olivier-Kango <olivierkango@gmail.com>
2022-10-07 16:10:00 -0700

    Add the option checkbox.checked

--- src/interative-list.js
+++ src/interative-list.js
@@ -3,13 +3,18 @@ import { toDoList } from './addremove.js';
 export default function updateInteractiveList(lists) {
   lists.forEach((elt) => {
     const checkbox = elt.querySelector('span').querySelector('.checkbox');
-    let check = false;
+    const content = elt.querySelector('span').querySelector('.para-input');
+    // let check = false;
     checkbox.addEventListener('click', () => {
-      check = !check;
       toDoList.forEach((list) => {
         if (list.index === Number(elt.id)) {
           const index = toDoList.indexOf(list);
-          toDoList[index].completed = check;
+          toDoList[index].completed = checkbox.checked;
+          if (checkbox.checked) {
+            content.style.textDecoration = 'line-through';
+          } else {
+            content.style.textDecoration = 'none';
+          }
           localStorage.setItem('toDoList', JSON.stringify(toDoList));
         }
       });
