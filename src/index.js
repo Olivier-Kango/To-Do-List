11b5269bd2e5d991f6affc0595c4dca3e8a344ac
Olivier-Kango <olivierkango@gmail.com>
2022-10-07 03:04:19 -0700

    Update add remove function

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
 /* eslint-disable no-unused-vars */
 import _, { add } from 'lodash';
 import './style.css';
-import addTask, { toDoList , addItem } from './addremove.js';
+import {
+  toDoList, addItem, removeTask, generateElement,
+} from './addremove.js';
 /* eslint-enable no-unused-vars */
 
 // Add and Remove
-const AddBtn = document.querySelector('.add-items-btn');
+const addBtn = document.querySelector('.add-items-btn');
+const input = document.querySelector('#text-input');
 
-AddBtn.onclick = addTask;
+toDoList.forEach((elt) => {
+  generateElement(elt.index, elt.completed, elt.description);
+  const lists = document.querySelectorAll('.ul-lists li');
+  removeTask(lists);
+});
 
-addItem.addEventListener('keypress', (enter) => {
+const triggerEvent = () => {
+  addItem(input.value, false);
+  input.value = '';
+  const lists = document.querySelectorAll('.ul-lists li');
+  removeTask(lists);
+};
+
+addBtn.addEventListener('click', () => {
+  triggerEvent();
+});
+
+input.addEventListener('keypress', (enter) => {
   if (enter.key === 'Enter') {
     enter.preventDefault();
-    AddBtn.onclick();
+    triggerEvent();
   }
 });
