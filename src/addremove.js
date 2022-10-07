5d989b01d31b05aca55a9ef968566a68ac1f4dd8
Olivier-Kango <olivierkango@gmail.com>
2022-10-07 12:47:18 -0700

    Fix linter errors

--- src/addremove.js
+++ src/addremove.js
@@ -14,6 +14,7 @@ export const generateElement = (index, completed, description) => {
 
   const checkbox = document.createElement('input');
   checkbox.setAttribute('type', 'checkbox');
+  checkbox.className = 'checkbox';
   checkbox.checked = completed;
   span.appendChild(checkbox);
 
